3991e137478fae02981ef69e5608b3e0b4127fb9
Prerak Mann <mannprerak2@gmail.com>
2020-03-13 21:05:58 +0530

    added benchmarks to readme

--- json_parser_ffi/README.md
+++ json_parser_ffi/README.md
@@ -1,5 +1,23 @@
 A demo library for JSON parsing using cJSON and dart:ffi
-
+# Benchmark
+```
+Test file size: 4.243239mb
+Time is average of 100 iterations...
+CJSON: (String to char*) + (char* to CJSON) + (CJSON to dart object)
+          25.14ms         +     51.42ms       +       963.11ms
+        Time = 1039ms
+dart:convert: (String to dart object)
+        Time = 35ms
+```
+```
+Test file size: 4.243239mb
+Time is average of 1 iterations...
+CJSON: (String to char*) + (char* to CJSON) + (CJSON to dart object)
+          33.0ms         +     50.0ms       +       1043.0ms
+        Time = 1126ms
+dart:convert: (String to dart object)
+        Time = 58ms
+```
 # Example (see example folder)
 
 Demo Run
@@ -24,4 +42,3 @@ JSON is parsed and stored as a C structure, let's iterate over it and make a dar
 Print the Map-
 {name: Prerak Mann, age: 21.0, nicknames: [{name: Mr. P, length: 5.0}, {name: Witch Burner, length: 12.0}, {name: mannprerak2, length: 11.0}]}
 ```
-
---
+++ json_parser_ffi/example/parse_benchmark.dart
@@ -0,0 +1,107 @@
+import 'dart:ffi';
+import 'dart:io';
+import 'dart:convert';
+import 'package:ffi/ffi.dart';
+import 'package:json_parser_ffi/json_parser_ffi.dart';
+
+// for cJSON
+int stringToCString = 0;
+int cStringToCJson = 0;
+int cJsonToDartObj = 0;
+
+// for dart:convert
+int stringToDartObj = 0;
+
+int count = 1;
+void main() {
+  var jsonString = getJsonAsString();
+
+  for (var i = 0; i < count; i++) {
+    cjson_parseJson(jsonString);
+    dartConvertParseJson(jsonString);
+  }
+
+  printDetails();
+}
+
+String getJsonAsString() {
+  var file = File('./example/big_json.json');
+  print('Test file size: ${file.lengthSync() / 1000000}mb');
+  print('Time is average of $count iterations...');
+  return file.readAsStringSync();
+}
+
+void cjson_parseJson(String jsonString) {
+  var stopwatch = Stopwatch()..start();
+  var cString = Utf8.toUtf8(jsonString);
+  stringToCString += stopwatch.elapsedMilliseconds;
+  stopwatch.reset();
+
+  var parsedJson = cJson_Parse(cString);
+  cStringToCJson += stopwatch.elapsedMilliseconds;
+  stopwatch.reset();
+
+  var obj = _convertCJsonToDartObj(parsedJson);
+  cJsonToDartObj += stopwatch.elapsedMilliseconds;
+  stopwatch.stop();
+}
+
+void dartConvertParseJson(String jsonString) {
+  var stopwatch = Stopwatch()..start();
+  var obj = json.decode(jsonString);
+  stringToDartObj += stopwatch.elapsedMilliseconds;
+  stopwatch.stop();
+}
+
+void printDetails() {
+  print('CJSON: (String to char*) + (char* to CJSON) + (CJSON to dart object)');
+  print(
+      '\t  ${stringToCString / count}ms         +     ${cStringToCJson / count}ms       +       ${cJsonToDartObj / count}ms');
+  print(
+      '\tTime = ${(stringToCString + cStringToCJson + cJsonToDartObj) ~/ count}ms');
+  print('dart:convert: (String to dart object)');
+  print('\tTime = ${stringToDartObj ~/ count}ms');
+}
+
+dynamic _convertCJsonToDartObj(Pointer<CJson> cjson) {
+  dynamic obj;
+  if (cjson.ref.type == Types.cJSON_Object) {
+    obj = <String, dynamic>{};
+
+    Pointer<CJson> ptr;
+    ptr = cjson.ref.child;
+    while (ptr != nullptr) {
+      var o = _convertCJsonToDartObj(ptr);
+      _addToObj(obj, o, ptr.ref.string);
+      ptr = ptr.ref.next;
+    }
+  } else if (cjson.ref.type == Types.cJSON_Array) {
+    obj = <dynamic>[];
+
+    Pointer<CJson> ptr;
+    ptr = cjson.ref.child;
+    while (ptr != nullptr) {
+      var o = _convertCJsonToDartObj(ptr);
+      _addToObj(obj, o);
+      ptr = ptr.ref.next;
+    }
+  } else if (cjson.ref.type == Types.cJSON_String) {
+    obj = Utf8.fromUtf8(cjson.ref.valueString);
+  } else if (cjson.ref.type == Types.cJSON_Number) {
+    obj = cjson.ref.valuedouble;
+  } else if(cjson.ref.type == Types.cJSON_True){
+    obj = true;
+  } else if(cjson.ref.type == Types.cJSON_False){
+    obj = false;
+  }
+
+  return obj;
+}
+
+void _addToObj(obj, o, [Pointer<Utf8> name]) {
+  if (obj is Map<String, dynamic>) {
+    obj[Utf8.fromUtf8(name)] = o;
+  } else if (obj is List<dynamic>) {
+    obj.add(o);
+  }
+}
